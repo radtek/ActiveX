b7c5c17839abc5179bbdf2ee8d5f185441bd4b26
Roman Davidenko <rid50@hotmail.com>
2016-02-12 20:48:28 +0300

    v100

---
+++ VideoActiveX/TestActiveXVideoControl/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TakePicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTake = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnTake);
+            this.panel1.Controls.Add(this.btnGet);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(547, 361);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnTake
+            // 
+            this.btnTake.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnTake.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnTake.BackColor = System.Drawing.Color.LightBlue;
+            this.btnTake.Enabled = false;
+            this.btnTake.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.btnTake.Location = new System.Drawing.Point(12, 323);
+            this.btnTake.Name = "btnTake";
+            this.btnTake.Size = new System.Drawing.Size(95, 23);
+            this.btnTake.TabIndex = 20;
+            this.btnTake.Text = "Take a Snap";
+            this.btnTake.UseVisualStyleBackColor = false;
+            this.btnTake.Click += new System.EventHandler(this.btnTake_Click);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGet.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnGet.BackColor = System.Drawing.Color.LightBlue;
+            this.btnGet.Enabled = false;
+            this.btnGet.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.Location = new System.Drawing.Point(266, 323);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(110, 23);
+            this.btnGet.TabIndex = 23;
+            this.btnGet.Text = "Get Picture";
+            this.btnGet.UseVisualStyleBackColor = false;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(183, 323);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 23);
+            this.textBox1.TabIndex = 21;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.WordWrap = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(294, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(241, 286);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 14);
+            this.label1.TabIndex = 22;
+            this.label1.Text = " Your Id";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 286);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGet;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 361);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTake;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
---
+++ VideoActiveX/TestActiveXVideoControl/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Security.Permissions;
+using System.Runtime.InteropServices;
+using System.Configuration;
+//using PublicWCFServices;
+
+namespace TakePicture
+{
+    public partial class Form1 : Form
+    {
+        //[DllImport("TiTGActiveXVideoControl.dll", CharSet = CharSet.Auto)]
+        //public static extern void StartAxVideoControl();
+
+        //[DllImport("TiTGActiveXVideoControl.dll", CharSet = CharSet.Auto)]
+        //public static extern void StopAxVideoControl();
+
+        //[DllImport("TiTGActiveXVideoControl.dll", CharSet = CharSet.Auto)]
+        //public static extern void GetIt(string id);
+
+        //[DllImport("TiTGActiveXVideoControl.dll", CharSet = CharSet.Auto)]
+        //public static extern void TakeIt(string id);
+
+        TiTGActiveXVideoControl.VideoControl control;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //textBox1.Text = "20095423";
+            textBox1.Text = "20005140";
+            //textBox1.Text = "20005232";
+
+            control = new TiTGActiveXVideoControl.VideoControl();
+
+            control.StartAxVideoControl();
+
+            btnTake.Enabled = true;
+            btnGet.Enabled = true;
+            btnTake.Focus();
+        }
+
+        private void btnTake_Click(object sender, EventArgs e)
+        {
+            int id;
+            if (textBox1.Text.Length == 0 || !Int32.TryParse(textBox1.Text, out id))
+            {
+                System.Windows.Forms.MessageBox.Show("Please enter a valid ID");
+                return;
+            }
+
+            btnTake.Enabled = false;
+            btnGet.Enabled = false;
+
+            btnTake.Enabled = true;
+            btnGet.Enabled = true;
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Background: " + Background);
+            //this.BackColor = ColorTranslator.FromHtml(Background);
+
+            int id;
+            if (textBox1.Text.Length == 0 || !Int32.TryParse(textBox1.Text, out id))
+            {
+                System.Windows.Forms.MessageBox.Show("Please enter a valid ID");
+                return;
+            }
+
+            btnTake.Enabled = false;
+            btnGet.Enabled = false;
+
+            try {
+                string ret = control.GetIt(id.ToString());
+                if (ret == "")
+                    ret = "Ok";
+
+                System.Windows.Forms.MessageBox.Show(ret);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+            btnTake.Enabled = true;
+            btnGet.Enabled = true;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
